f60908d6bfcc6ee76c4b0f3439c86e39597575c5
Ian Palmgren <ianmpal@gmail.com>
2015-10-13 12:13:37 -0700

    Tuples working

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+
+// Homework: Build a Calculator
+
 func add(num1: Double, num2: Double) -> Double {
     return num1 + num2
 }
@@ -24,7 +27,7 @@ func div(num1: Double, num2: Double) -> Double {
     return num1 / num2
 }
 
-//
+
 //print(add(2, num2: 3))
 //print(sub(5, num2: 3))
 //print(mult(4, num2: 8))
@@ -38,10 +41,12 @@ func performOp(num1: Double, num2: Double, op: (Double, Double) -> Double) -> Do
 
 //print(performOp(10, num2: 5, op: mult))
 
+// Homework: Array Fun
+
 func addArray(numbers: [Int]) -> Int {
     var total: Int = 0
     for num in numbers {
-        total *= num
+        total += num
     }
     return total
 }
@@ -69,12 +74,42 @@ func avgArray(numbers: [Int]) -> Double {
 
 var numbers: [Int] = [2, 3 , 4, 5]
 
-print(avgArray(numbers))
+//print(avgArray(numbers))
 
 
-func performOp(num1: Double, num2: Double, op: (Double, Double) -> Double) -> Double {
-    return op(num1, num2)
+func performOp(arr1: [Int], op: ([Int]) -> Int) -> Int {
+    return op(arr1)
 }
+
+//print(performOp(numbers, op: countArray))
+
+
+// Homework: Points
+
+let point1 = (1,2)
+let point2 = (3,4)
+
+
+func addPoints(point1: (Int, Int), point2: (Int, Int)) -> (newX: Int, newY: Int) {
+    let newPoint = (point1.0 + point2.0, point1.1 + point2.1)
+    return newPoint
+}
+
+func subPoints(point1: (Int, Int), point2: (Int, Int)) -> (newX: Int, newY: Int) {
+    let newPoint = (point1.0 - point2.0, point1.1 - point2.1)
+    return newPoint
+}
+
+print(addPoints(point1, point2: point2))
+print(subPoints(point1, point2: point2))
+
+
+
+
+
+
+
+
 
 
 
