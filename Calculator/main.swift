f7785cd19c6363215154bc6872a359a052945379
Ian Palmgren <ianmpal@gmail.com>
2015-10-13 10:01:00 -0700

    First push

---
+++ Calculator/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Ian Palmgren on 10/13/15.
+//  Copyright © 2015 Ian Palmgren. All rights reserved.
+//
+
+import Foundation
+
+func add(num1: Double, num2: Double) -> Double {
+    return num1 + num2
+}
+
+func sub(num1: Double, num2: Double) -> Double {
+    return num1 - num2
+}
+
+func mult(num1: Double, num2: Double) -> Double {
+    return num1*num2
+}
+
+func div(num1: Double, num2: Double) -> Double {
+    return num1 / num2
+}
+
