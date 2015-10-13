da9903839538ecd4e8c6fbf46cd1f0ebfe0ccec9
Ian Palmgren <ianmpal@gmail.com>
2015-10-13 11:21:13 -0700

    Halway through

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -24,3 +24,61 @@ func div(num1: Double, num2: Double) -> Double {
     return num1 / num2
 }
 
+//
+//print(add(2, num2: 3))
+//print(sub(5, num2: 3))
+//print(mult(4, num2: 8))
+//print(div(32,num2: 8))
+
+func performOp(num1: Double, num2: Double, op: (Double, Double) -> Double) -> Double {
+    return op(num1, num2)
+}
+
+
+
+//print(performOp(10, num2: 5, op: mult))
+
+func addArray(numbers: [Int]) -> Int {
+    var total: Int = 0
+    for num in numbers {
+        total *= num
+    }
+    return total
+}
+
+func multArray(numbers: [Int]) -> Int {
+    var total: Int = 1
+    for num in numbers {
+        total *= num
+    }
+    return total
+}
+
+func countArray(numbers: [Int]) -> Int {
+    return numbers.count
+}
+
+func avgArray(numbers: [Int]) -> Double {
+    var total: Double = 0
+    for num in numbers {
+        total += Double(num)
+    }
+    return total / Double(numbers.count)
+}
+
+
+var numbers: [Int] = [2, 3 , 4, 5]
+
+print(avgArray(numbers))
+
+
+func performOp(num1: Double, num2: Double, op: (Double, Double) -> Double) -> Double {
+    return op(num1, num2)
+}
+
+
+
+
+
+
+
